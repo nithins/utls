0bd20998d61313202c9bfa514ac1597043bc65a3
Nithin Shivashankar <nithin19484@gmail.com>
2010-05-05 18:34:47 +0530

    logutils change

--- include/logutil.h
+++ include/logutil.h
@@ -47,7 +47,7 @@ std::cerr <<"WARNING  ::" << logstr <<std::endl
 //std::cout <<"LOG V    ::" << logstr <<std::endl
 
 #define _LOG_VAR(var)\
-_LOG(#var<<" = "<<var);
+std::cout<<#var<<" = "<<var<<"\n";
 
 #define _LOG_FILE_N_FUNC() _LOG(__FILE__<<"::"<<__func__);
 
