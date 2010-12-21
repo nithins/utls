201901f51804c23344777ec72b6c8d9594d9b2d9
Nithin Shivashankar <nithin19484@gmail.com>
2010-12-21 12:44:10 +0530

    minor: logutil log_var flush immediate

--- include/logutil.h
+++ include/logutil.h
@@ -47,7 +47,7 @@ std::cerr <<"WARNING  ::" << logstr <<std::endl
 //std::cout <<"LOG V    ::" << logstr <<std::endl
 
 #define _LOG_VAR(var)\
-std::cout<<#var<<" = "<<var<<"\n";
+std::cout<<#var<<" = "<<var<<std::endl;
 
 #define _LOG_FILE_N_FUNC() _LOG(__FILE__<<"::"<<__func__);
 
